69e1f962272f7412b6b198d5aef742684040c57b
Mo7amed7afezAmer <mo7amed7afez0120305@gmail.com>
2023-07-16 15:32:56 +0300

    delete folder

--- frontend/src/components/MessageBox.jsx
+++
@@ -1,9 +0,0 @@
-const MessageBox = (props) => {
-    return (
-        <div className={ `msg-box ${props.msg !== ""? "open" : null }`} id="msgBox">
-            { props.msg }
-        </div>
-    )
-}
-
-export default MessageBox;
